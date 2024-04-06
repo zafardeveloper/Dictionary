bcad06fb4baab171efc0149280c402f8ba3dd81f
Zafar <zafarbobojonov05032005@gmail.com>
2024-04-06 14:42:52 +0500

    Dictionary Project V1

---
+++ app/src/main/java/com/example/bottomnavigation_practise/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.bottomnavigation_practise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.bottomnavigation_practise.screen.FirstFragment
+import com.example.bottomnavigation_practise.screen.SecondFragment
+import com.example.bottomnavigation_practise.screen.ThirdFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        replaceFragment(FirstFragment())
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation_view).setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.item_home -> replaceFragment(FirstFragment())
+                R.id.item_people -> replaceFragment(SecondFragment())
+                R.id.item_settings -> replaceFragment(ThirdFragment())
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+           .replace(R.id.nav_host_fragment, fragment)
+           .commit()
+    }
+}
