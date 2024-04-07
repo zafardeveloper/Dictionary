e0fba0dbbe635db5f3db23ea1f9ae3e93f38d67b
Zafar <zafarbobojonov05032005@gmail.com>
2024-04-07 18:51:44 +0500

    added SplashScreen and animations

--- app/src/main/java/com/example/bottomnavigation_practise/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigation_practise/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.bottomnavigation_practise.screen.FirstFragment
 import com.example.bottomnavigation_practise.screen.SecondFragment
+import com.example.bottomnavigation_practise.screen.SplashFragment
 import com.example.bottomnavigation_practise.screen.ThirdFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -13,13 +14,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        replaceFragment(FirstFragment())
+        replaceFragment(SplashFragment())
 
         findViewById<BottomNavigationView>(R.id.bottom_navigation_view).setOnItemSelectedListener {
             when (it.itemId) {
-                R.id.item_home -> replaceFragment(FirstFragment())
-                R.id.item_people -> replaceFragment(SecondFragment())
-                R.id.item_settings -> replaceFragment(ThirdFragment())
+                R.id.item_home -> postDelayed { replaceFragment(FirstFragment()) }
+                R.id.item_people -> postDelayed { replaceFragment(SecondFragment()) }
+                R.id.item_settings -> postDelayed { replaceFragment(ThirdFragment()) }
             }
             true
         }
@@ -27,8 +28,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-           .replace(R.id.nav_host_fragment, fragment)
-           .commit()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        // Добавьте анимации перехода
+        fragmentTransaction.setCustomAnimations(
+            R.anim.from_right,  // анимация при входе нового фрагмента
+            R.anim.to_left,  // анимация при выходе старого фрагмента
+            R.anim.from_left,  // анимация при входе старого фрагмента (при нажатии кнопки "Назад")
+            R.anim.to_right)  // анимация при выходе нового фрагмента (при нажатии кнопки "Назад")
+        fragmentTransaction.replace(R.id.nav_host_fragment, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
+    private fun postDelayed(action: () -> Unit) {
+        findViewById<BottomNavigationView>(R.id.bottom_navigation_view).postDelayed({
+            action.invoke()
+        }, 300)
     }
 }
