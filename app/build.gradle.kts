15358aacfad34b2a39ed1736a09c782a437560fc
Zafar <zafarbobojonov05032005@gmail.com>
2024-04-13 16:26:54 +0500

    themeSwitcher in lifeCycleScope

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
